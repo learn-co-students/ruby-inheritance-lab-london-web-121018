f016ef30d42d6c738ca6c19cefc37c1b68e75d4a
timtan93 <timtan93@gmail.com>
2018-12-18 14:34:12 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+class Student < User
 
+attr_writer :string
+
+  def initialize(knowledge = [])
+    @knowledge = knowledge
+  end
+
+  def learn (string)
+    @knowledge << string
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
